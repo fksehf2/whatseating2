61893247acc762c60f3c1596c65dcbbfc1f34598
fksehf2 <fkstpdl2@gmail.com>
2022-04-17 14:30:54 +0900

    add

---
+++ src/main/java/com/test/one/StudyApplication.java
@@ -0,0 +1,15 @@
+package com.test.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StudyApplication.class, args);
+        System.out.println("test");
+    }
+
+}
